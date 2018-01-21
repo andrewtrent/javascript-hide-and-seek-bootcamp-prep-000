340bbdebfe68cd542c09d011337afec4c4c4941d
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-21 05:44:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ return listQueue
 
 function deepestChild(){
   var gn = document.querySelector('div + div');
-  var gnc = [gn]
-for(var i = 0; i < 10; i++){
-  if(gnc[0].children === true){
-    gnc.push(gnc[0].children[0])
-    gnc.shift()
-  } else{
-  return gnc[0];
+  var gnc = [gn];
+  for(var i = 0; i < 10; i++){
+    if(gnc[0].children === true){
+      gnc.push(gnc[0].children[0])
+      gnc.shift()
+    } else{
+    return gnc[0];
+    }
   }
-}
 }
