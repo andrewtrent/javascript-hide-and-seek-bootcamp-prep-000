34f655607f031abe7512000f582f81139492e053
Andrew Trent <andrewtrentdesign@gmail.com>
2018-01-21 04:57:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n){
+  var listCollection = document.querySelectorAll(".ranked-list li");
+return listCollection
+}
+increaseRankBy(2)
+function deepestChild(){
+  document.querySelector()
+}
